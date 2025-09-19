e4424b033349ab5b1f431e10e5bb8879837922d6
nekun <gameplayzaid@gmail.com>
2025-09-19 08:10:18 +0800

    countdown

--- script.js
+++ script.js
@@ -54,3 +54,26 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+const countdownDate = new Date("2026-02-05T00:00:00").getTime();
+
+const x = setInterval(function () {
+  const now = new Date().getTime();
+
+  const distance = countdownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days.toString().padStart(2, '0');
+  document.getElementById("hours").innerText = hours.toString().padStart(2, '0');
+  document.getElementById("minutes").innerText = minutes.toString().padStart(2, '0');
+  document.getElementById("seconds").innerText = seconds.toString().padStart(2, '0');
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.querySelector(".countdownbox").innerHTML = "<p>Waktu telah tiba!</p>";
+  }
+}, 1000);
